24287a3bb7e7cf22478a1347e3011345632d9681
Pravin Kumar Dubey <100483368+pravindubey15@users.noreply.github.com>
2024-04-07 10:58:58 +0530

    Initial commit

---
+++ PdfToTxtConversion/src/main/java/com/example/PdfToTxtConversion/PdfController.java
@@ -0,0 +1,48 @@
+package com.example.PdfToTxtConversion;
+
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.FileSystemNotFoundException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/dubey")
+public class PdfController {
+    @GetMapping("/get")
+    public String getAll(){
+        return "Hello";
+    }
+
+    @PostMapping("/pdftotxt")
+    public String pdfToTxt(@RequestParam("file") MultipartFile file) throws IOException{
+
+        String name=file.getOriginalFilename();
+        String pathx="C:\\Users\\SCOREME-LT-PRAVIN\\Desktop\\S's work\\";
+        Path filePath = Paths.get(pathx, name);
+        if (!Files.exists(filePath.getParent())) {
+            Files.createDirectories(filePath.getParent());
+        }
+        // Save the file to the specific directory
+        file.transferTo(filePath.toFile());
+        PDDocument pd=Loader.loadPDF(new File(String.valueOf(filePath)));
+            PDFTextStripper stripper = new PDFTextStripper();
+           String text = stripper.getText(pd);
+            File outputFile = new File(filePath+".txt");
+            FileWriter writer = new FileWriter(outputFile);
+            writer.write(text);
+            writer.close();
+
+                return "Successfully:-  "+name+" "+ filePath +"converted  "+writer;
+    }
+}
---
+++ PdfToTxtConversion/src/main/java/com/example/PdfToTxtConversion/PdfToTxtConversionApplication.java
@@ -0,0 +1,13 @@
+package com.example.PdfToTxtConversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfToTxtConversionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfToTxtConversionApplication.class, args);
+	}
+
+}

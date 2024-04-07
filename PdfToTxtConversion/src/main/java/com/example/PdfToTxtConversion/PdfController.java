65c788f98888932016b7d48a24d4a64f1a73060a
Pravin Kumar Dubey <100483368+pravindubey15@users.noreply.github.com>
2024-04-07 17:39:37 +0530

    PDF to TXT Conversion

--- PdfToTxtConversion/src/main/java/com/example/PdfToTxtConversion/PdfController.java
+++ PdfToTxtConversion/src/main/java/com/example/PdfToTxtConversion/PdfController.java
@@ -14,37 +14,42 @@ import java.nio.file.FileSystemNotFoundException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/dubey")
 public class PdfController {
     @GetMapping("/get")
-    public String getAll(){
+    public String getAll() {
         return "Hello";
     }
 
     @PostMapping("/pdftotxt")
-    public String pdfToTxt(@RequestParam("file") MultipartFile file) throws IOException{
-        UUID uuid=UUID.randomUUID();
-        String refId=uuid.toString();
+    public List<String> pdfToTxt(@RequestParam("file") List<MultipartFile> file) throws IOException {
+        ArrayList<String> refIds=new ArrayList<>();
+        for(MultipartFile files:file) {
+            UUID uuid = UUID.randomUUID();
+            String refId = uuid.toString();
+            String name = files.getOriginalFilename();
+            String pathx = "C:\\Users\\SCOREME-LT-PRAVIN\\Desktop\\S's work\\";
+            Path filePath = Paths.get(pathx, name);
 
-        String name=file.getOriginalFilename();
-        String pathx="C:\\Users\\SCOREME-LT-PRAVIN\\Desktop\\S's work\\";
-        Path filePath = Paths.get(pathx, name);
-        if (!Files.exists(filePath.getParent())) {
-            Files.createDirectories(filePath.getParent());
-        }
-        // Save the file to the specific directory
-        file.transferTo(filePath.toFile());
-        PDDocument pd=Loader.loadPDF(new File(String.valueOf(filePath)));
+            if (!Files.exists(filePath.getParent())) {
+                Files.createDirectories(filePath.getParent());
+            }
+            // Save the file to the specific directory
+            files.transferTo(filePath.toFile());
+            PDDocument pd = Loader.loadPDF(new File(String.valueOf(filePath)));
             PDFTextStripper stripper = new PDFTextStripper();
-           String text = stripper.getText(pd);
-            File outputFile = new File(pathx+refId+".txt");
+            String text = stripper.getText(pd);
+            File outputFile = new File(pathx + refId + ".txt");
             FileWriter writer = new FileWriter(outputFile);
             writer.write(text);
             writer.close();
-
-                return "Successfully:-  "+name+" "+ pathx +"converted  "+writer+"\n refId: "+refId;
-    }
+            refIds.add(refId);
+        }
+            return refIds;
+        }
 }
